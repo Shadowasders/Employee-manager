d28ad58b078f07f0568ef3d13334ae45f0d34a95
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-06-19 02:33:21 -0400

    added basic schema file structure

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-
+const menuList = ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update employee information"]
 
 const interface = [
     {
         type: 'list',
         name: 'mainmenu',
-        message: 'Please choose from the following options',
+        message: 'Welcome! Please choose from the following options',
         choices: menuList,
     }
 ]
@@ -15,3 +15,5 @@ const interface = [
 function init() {
     inquirer.createPromptModule(interface);
 }
+
+init();
