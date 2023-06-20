677b01a1387b769f75224f13e7e3f3929e120e39
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-06-19 21:15:45 -0400

    reorginized schema files

--- db/departments.sql
+++ db/departments.sql
@@ -1,8 +1,25 @@
-    DROP DATABASE IF EXISTS departments_db;
-    CREATE DATABASE departments_db;
-    USE departments_db;
+    DROP DATABASE IF EXISTS company_db;
+    CREATE DATABASE company_db_db;
+    USE company_db;
 
     CREATE TABLE departments (
-        id INT NOT NULL,
+        id INT AUTO_INCREMENT NOT NULL,
         name VARCHAR(100) NOT NULL 
     );
+
+    CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(100), NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    salary INT NOT NULL,
+);
