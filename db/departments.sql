1a7643f0919d080d5b98daa7c9c5b7d28d35a2c1
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-06-24 13:55:14 -0400

    reorginized file structure, added switch statement for menu options

--- db/departments.sql
+++ db/departments.sql
@@ -1,25 +1,27 @@
     DROP DATABASE IF EXISTS company_db;
-    CREATE DATABASE company_db_db;
+    CREATE DATABASE company_db;
     USE company_db;
 
     CREATE TABLE departments (
         id INT AUTO_INCREMENT NOT NULL,
-        name VARCHAR(100) NOT NULL 
+        name VARCHAR(100) NOT NULL, 
+        PRIMARY KEY (id)
     );
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    departments_id INT NOT NULL,
+    salary INT NOT NULL
+    FOREIGN KEY (departments_id)
+    REFERENCES departments(id)
+);
 
     CREATE TABLE employees (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     role VARCHAR(100) NOT NULL,
-    department VARCHAR(100) NOT NULL,
     salary INT NOT NULL,
-    manager VARCHAR(100), NOT NULL
-);
-
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    department VARCHAR(100) NOT NULL,
-    salary INT NOT NULL,
+    manager VARCHAR(100)
 );
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+require('dotenv').config() 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const db = require("./lib/index");
 
+// db.viewRoles()
 const menuList = ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update employee information"]
-
 const interface = [
     {
         type: 'list',
-        name: 'mainmenu',
+        name: 'menu',
         message: 'Welcome! Please choose from the following options',
         choices: menuList,
-    }
+    },
 ]
 
 function init() {
-    inquirer.createPromptModule(interface);
+    inquirer.prompt(interface)
+    .then (answers => {
+        console.log(answers);
+        switch(answers.menu){
+            case "View all departments":
+            const dpts = db.viewDepartments()
+            console.table(dpts);
+        }
+    })
 }
 
 init();
